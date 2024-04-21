6bcba5df4c9ad46a416bd13b4ae90b4333c66b46
Windyl Joseph A. Khu <windylkhu@gmail.com>
2024-04-21 18:35:07 +0800

    added media

--- components/Welcome.js
+++ components/Welcome.js
@@ -7,75 +7,86 @@ const App=()=>{
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled(previousState => !previousState);
   return (
-    <View style={{backgroundImage: `url("../assets/Background.png")`, height:'100%', alignContent:'center'}}>
-        <ImageBackground source={background} resizeMode="cover" style={style.image}>
+    <View style={styles.container}>
+        <ImageBackground source={background} resizeMode="cover" style={styles.image}>
             <View style={{height:'33%', justifyContent: 'center', alignItems:'center'}}>
-                <Text style={[{fontSize: 100},style.glacialindifferenceBold]}>INNOVATION</Text>
+                <Text style={[{fontSize: 100},styles.glacialindifferenceBold]}>INNOVATION</Text>
                 <Text style={{fontSize: 50}}>PRIORITY AREAS</Text>
             </View>
             
             <View style={{height:'33%', flexDirection: 'row', justifyContent: 'center', alignItems:'center'}}>
                 <Pressable
-                  onPress={toggleSwitch}>
+                  onPress={toggleSwitch}
+                  style={{}}>
+                  <Text>Learning and Education</Text>
                   <Image
                     source={require('../assets/icons/1. Learning and Education.png')}
-                    style={[style.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
+                    style={[styles.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
                 </Pressable>
                 <Pressable
                   onPress={toggleSwitch}>
+                  <Text>Health and Wellbeing</Text>
                   <Image
                     source={require('../assets/icons/2. Health and Wellbeing.png')}
-                    style={[style.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
+                    style={[styles.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
                 </Pressable>
                 <Pressable
                   onPress={toggleSwitch}>
+                  <Text>Food and Agribusiness</Text>
                   <Image
                     source={require('../assets/icons/3. Food and Agribusiness.png')}
-                    style={[style.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
+                    style={[styles.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
                 </Pressable>
                 <Pressable
                   onPress={toggleSwitch}>
+                  <Text>Finance</Text>
                   <Image
                     source={require('../assets/icons/4. Finance.png')}
-                    style={[style.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
+                    style={[styles.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
                 </Pressable>
                 <Pressable
                   onPress={toggleSwitch}>
+                  <Text>Manufacturing and Trade</Text>
                   <Image
                     source={require('../assets/icons/5. Manufacturing and Trade.png')}
-                    style={[style.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
+                    style={[styles.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
                 </Pressable>
             </View>
             <View style={{height:'33%', flexDirection: 'row', justifyContent: 'center', alignItems:'center'}}>
                 <Pressable
                   onPress={toggleSwitch}>
+                  <Text>Transportation and Logistics</Text>
                   <Image
                     source={require('../assets/icons/6. Transportation and Logistics.png')}
-                    style={[style.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
+                    style={[styles.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
                 </Pressable>
                 <Pressable
                   onPress={toggleSwitch}>
+                  <Text>Public Administration</Text>
                   <Image
                     source={require('../assets/icons/7. Public Administration.png')}
-                    style={[style.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
+                    style={[styles.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
                 </Pressable>
                 <Pressable
                   onPress={toggleSwitch}>
+                  <Text>Security and Defense</Text>
                   <Image
                     source={require('../assets/icons/8. Security and Defense.png')}
-                    style={[style.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
+                    style={[styles.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
                 </Pressable>
                 <Pressable
                   onPress={toggleSwitch}>
+                  <Text>Energy</Text>
                   <Image
                     source={require('../assets/icons/9. Energy.png')}
-                    style={[style.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
+                    style={[styles.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
                 </Pressable>
                 <Pressable
                   onPress={toggleSwitch}>
+                  <Text>Blue Water and Economy</Text>
                   <Image
                     source={require('../assets/icons/10. Blue Water and Economy.png')}
-                    style={[style.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
+                    style={[styles.buttonDims, {backgroundColor: isEnabled?'green':'00FFFFFF'},{borderRadius:75}]}/>
                 </Pressable>
             </View>  
         </ImageBackground>
@@ -83,7 +94,10 @@ const App=()=>{
   )
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   buttonDims: {
     width: 100,
     height: 100
